73e6ea11d6563cffec2a9a6e6e09691e04715389
anshu957 <32687084+anshu957@users.noreply.github.com>
2024-10-30 14:49:28 -0400

    Create submit_infer_cpu.sh

---
+++ src/VAME/submit_infer_cpu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=vame_infer8_groom
+#SBATCH --time=01:00:00
+#SBATCH --array=0-133
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+
+# Create sbatch_logs directory if it doesn't exist in the project root directory/logs
+LOG_DIR="$(dirname "$(dirname "${BASH_SOURCE[0]}")")/logs/sbatch_logs"
+mkdir -p "$LOG_DIR"
+
+# output and error paths to point to the log directory
+#SBATCH --output="$LOG_DIR/grooming8k_setup_%A_%a.out"
+#SBATCH --error="$LOG_DIR/grooming8k_setup_%A_%a.err"
+
+# Calculate the starting video index for the current job
+START=$((SLURM_ARRAY_TASK_ID * 10))
+
+# Run the script
+python3 -u vame_infer_cpu.py $START 10
